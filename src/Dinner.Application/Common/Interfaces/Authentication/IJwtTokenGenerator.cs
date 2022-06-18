77af1b583b5a50a06537dc1a4632ac3b5656b889
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-06-18 09:44:29 +0200

    Add jwt token generator

---
+++ src/Dinner.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Dinner.Domain.Entities;
+
+namespace Dinner.Application.Common.Interfaces.Authentication
+{
+    public interface IJwtTokenGenerator
+    {
+        public string GenerateToken(User user);
+    }
+}
