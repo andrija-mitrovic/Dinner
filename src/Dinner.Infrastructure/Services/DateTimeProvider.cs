34dba72b667b60ac18e0c60d0700f6429185fe15
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-06-19 09:07:30 +0200

    Add datetime provider

---
+++ src/Dinner.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using Dinner.Application.Common.Interfaces.Services;
+
+namespace Dinner.Infrastructure.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
