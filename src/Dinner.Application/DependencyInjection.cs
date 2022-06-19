2a8c05de6096a0007610a7bb322bf8308372bb7a
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-06-19 09:09:09 +0200

    Add dependency injection in application layer

---
+++ src/Dinner.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Dinner.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dinner.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+            return services;
+        }
+    }
+}
