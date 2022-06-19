ec297c4adc0cc3e403c2ad149198903c21617b47
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-06-19 09:07:50 +0200

    Add user repository

---
+++ src/Dinner.Application/Common/Interfaces/Persistence/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Dinner.Domain.Entities;
+
+namespace Dinner.Application.Common.Interfaces.Persistence
+{
+    public interface IUserRepository
+    {
+        User? GetUserByEmail(string email);
+        void Add(User user);
+    }
+}
